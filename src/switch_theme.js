303e26fc698ed469ae4bbe7d605fdb156e421b47
SaymenAbbas <saymenabbas@gmail.com>
2025-07-14 02:42:36 +0200

    Changed Light/Black button design, finished Project and Articles page.

--- src/switch_theme.js
+++ src/switch_theme.js
@@ -9,10 +9,10 @@ function toggleTheme () {
     if (currentTheme === "light") {
         // removes data-theme="light" on html tag
         root.removeAttribute("data-theme");
-        toggleButton.innerHTML =" <em>Light Mode Enable</em>"
+        toggleButton.innerHTML =" Light Mode Enable"
     } else {
         root.setAttribute("data-theme", "light")
-        toggleButton.innerHTML =" <em>Dark Mode Enable</em>"
+        toggleButton.innerHTML =" Dark Mode Enable"
     }
 }
 
