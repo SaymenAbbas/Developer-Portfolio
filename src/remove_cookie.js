8fb4b724c702f8a9cbcb6eb357b189d888b54b13
SaymenAbbas <saymenabbas@gmail.com>
2025-07-22 22:07:00 +0200

    Saving Cookie in localstorage and fixed issue of cookie flashing if switching pages. Fixed the cookie flashing by checking localStorage before showing popup. It was flipped.

--- src/remove_cookie.js
+++ src/remove_cookie.js
@@ -1,10 +1,20 @@
 
-function removeCookie() {
-    const cookie_popup = document.getElementsByClassName("cookie-wrapper")[0]
+const savedCookie = localStorage.getItem('display');
 
-    if (cookie_popup.style.display === "none") {
-        cookie_popup.style.display = "block";
-    } else {
-        cookie_popup.style.display = "none";
-    }
+const cookie_popup = document.getElementsByClassName('cookie-wrapper')[0]
+
+if (!savedCookie) {
+    // First time visit: popup is shown
+    cookie_popup.style.display = 'block';
+} else if (savedCookie === 'none') {
+    cookie_popup.style.display = 'none';
+} else {
+    // Fallback: show popup
+    cookie_popup.style.display = 'block'
+}
+
+function removeCookie() {
+    cookie_popup.style.display = 'none';
+    localStorage.setItem('display', 'none');
+
 }
